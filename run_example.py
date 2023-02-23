9c82e105ef0a0bcdb57e80b76403e4d3ed74befe
Luis Esteban Rodriguez <rodriguezjluis0@gmail.com>
2023-02-23 17:49:57 +0100

    run_example.py: dynamic module discover

--- run_example.py
+++ run_example.py
@@ -6,10 +6,6 @@ import sys
 
 VERSION = "1.0.0"
 
-EXAMPLES = {
-    "1": "guessing_game",
-}
-
 def get_usage():
     usage = """
         ***********************************************************
@@ -38,24 +34,31 @@ def get_usage():
         """
     return usage
 
+def get_current_level_dirs():
+    return list(filter(lambda x: x not in ['target', '.git', 'src'], next(os.walk('.'))[1]))
+
+def generate_examples():
+    return dict( ( (str (k+1 ), v ) for k,v in zip( range( len( get_current_level_dirs() ) ), get_current_level_dirs() ) ) )
+
 def main():
     choice = sys.argv[1] if len(sys.argv) > 1 else 0
-    
     if choice == "-h" or choice == "--help":
         print(get_usage())
         sys.exit(0)
     elif choice == "--version":
         print(VERSION)
         sys.exit(0)
+        
+    examples = generate_examples()
     
-    if choice not in list(EXAMPLES.keys()):
+    if choice not in list(examples.keys()):
         print("[!] Select a valid choice.")
         sys.exit(-1)
     
-    print("[+] Running example {}".format(EXAMPLES[choice]))
+    print("[+] Running example {}".format(examples[choice]))
 
     try:
-        path = "{}/{}/Cargo.toml".format(os.getcwd(), EXAMPLES[choice])
+        path = "{}/{}/Cargo.toml".format(os.getcwd(), examples[choice])
         subprocess.run(["cargo", "run", "--manifest-path", path])
     except KeyboardInterrupt:
         print("[!] Exiting...")
